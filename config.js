16833757c7b621eadc5349c418e8ba611978147f
Daniel Young <daniel.young@bluefrontier.co.uk>
2020-08-03 14:59:34 +0100

    fix: fixing transmission retries

--- config.js
+++ config.js
@@ -6,5 +6,12 @@ module.exports = new oop.Config({
     relayInputQ: "OOP_RELAY_INPUT_Q",
     exchangeName: "OOP_EXCHANGE_NAME",
     recursiveTemprQ: "OOP_RECURSIVE_TEMPR_Q",
-    coreResponseQ: "OOP_CORE_RESPONSE_Q"
+    coreResponseQ: "OOP_CORE_RESPONSE_Q",
+    endpointsExchange: "OOP_ENDPOINTS_EXCHANGE_NAME",
+    endpointsQ: "OOP_ENDPOINT_Q",
+    retryLimit: {
+        name: "OOP_TRANSMISSION_RETRY_LIMIT",
+        optional: true,
+        default: 0,
+    }
 });
